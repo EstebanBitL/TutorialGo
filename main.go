d0fc18acdde9caa04948896049bd4439781dad19
EstebanBitL <esteban.lescano@bitlogic.io>
2023-06-11 08:19:37 -0300

    siguiente commit

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"tutorialGo/variables"
 	//go/src/tutorialGo/variables
 )
 
 func main() {
 	//variables.MuestroVariables()
-	variables.RestoVariables()
+	//variables.RestoVariables()
+	estado, texto := variables.ConviertoaTexto(1566)
+	fmt.Println(estado, texto)
 }
