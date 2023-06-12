c6c65dfbfadecb0361706bd9957e8d8451d181ef
EstebanBitL <esteban.lescano@bitlogic.io>
2023-06-12 12:19:17 -0300

    Siguiente commit

---
+++ ejercicios/ejercicios01.go
@@ -0,0 +1,15 @@
+package ejercicios
+
+import "strconv"
+
+func ConvNumericos(texto string) (int, string) {
+	num, err := strconv.Atoi(texto)
+	if err != nil {
+		return 0, "Hubo un error " + err.Error()
+	}
+	if num > 100 {
+		return num, "es mayor a 100"
+	} else {
+		return num, "es menor a 100"
+	}
+}
--- main.go
+++ main.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"fmt"
-	"tutorialGo/variables"
-	//go/src/tutorialGo/variables
+	"tutorialGo/ejercicios"
+	//"tutorialGo/variables"
 )
 
 func main() {
+
 	//variables.MuestroVariables()
 	//variables.RestoVariables()
-	estado, texto := variables.ConviertoaTexto(1566)
-	fmt.Println(estado, texto)
+	//estado, texto := variables.ConviertoaTexto(1566)
+	//fmt.Println(estado, texto)
+	/*if os := runtime.GOOS; os == "linux" || os == "windows" {
+		fmt.Println("Esta en linux")
+	} else {
+		fmt.Println("Esta en otro sistema operativo")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esta en linux")
+	case "darwin":
+		fmt.Println("Esta en darwin")
+	default:
+		fmt.Println("%s \n", os)
+	}*/
+
+	numero, texto := ejercicios.ConvNumericos("holaa")
+	fmt.Println(numero, texto)
 }
