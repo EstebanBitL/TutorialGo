dbc00ccb807f70b4a4b3c0820ef04755646258f6
EstebanBitL <esteban.lescano@bitlogic.io>
2023-06-26 08:23:00 -0300

    Siguiente commit

--- funciones/funcionesAnonimas.go
+++ funciones/funcionesAnonimas.go
@@ -7,8 +7,7 @@ func Calculos() {
 	var numero4 int = 243
 
 	suma := func(numero1 int, numero2 int) int {
-		return numero1 + numero2
-
+		return numero1 + numero2 + numero3 + numero4 
 	}
 	fmt.Println(suma(10, 25))
 }
